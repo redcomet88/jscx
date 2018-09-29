3ac19caa49a408f494b872ba73815d9fa15d98c3
redcomet88 <626206333@qq.com>
2018-09-29 13:51:25 +0800

    update comments of BaseService
    Signed-off-by: redcomet88 <626206333@qq.com>

--- src/main/java/jssvc/base/service/BaseService.java
+++ src/main/java/jssvc/base/service/BaseService.java
@@ -6,22 +6,23 @@ import java.util.List;
 
 public interface BaseService {
 
-
-    /***************************************************************************
-     * @Description 根据类型和键值获取对应常量
-     * @param type 类型、enKey 键值
-     * @return user 常量
-     * @author redcomet @date 2018-09-29 @reason 新增
-     * @throws SQLException
-     **************************************************************************/
+    /**
+     * @description: 
+     *
+     * @author: redcomet
+     * @param: [type, enKey]
+     * @return: java.lang.String        
+     * @create: 2018/9/29 
+     **/
     String getConstant(String type, String enKey) throws SQLException;
 
-    /***************************************************************************
-     * @Description 根据类型和名称值获取对应常量
-     * @param type 类型、enKey 键值
-     * @return 常量
-     * @author redcomet @date 2018-09-29 @reason 新增
-     * @throws SQLException
-     **************************************************************************/
+   /**
+    * @description: 
+    *
+    * @author: redcomet
+    * @param: [type, name]
+    * @return: java.lang.String        
+    * @create: 2018/9/29 
+    **/
     String getConstantByName(String type, String name) throws SQLException;
 }
